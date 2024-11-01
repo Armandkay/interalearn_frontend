693decda5b27e2896f898f976af23ea07991ed78
Armandkay <a.kayiranga1@alustudent.com>
2024-11-01 03:18:02 -0700

    Blog

---
+++ interalearn_frontend/src/pages/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Read our latest blog posts.</p>
+    </div>
+  );
+};
+
+export default Blog;
+
