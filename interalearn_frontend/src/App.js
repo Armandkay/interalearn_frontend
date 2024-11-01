5f5101f903aebef7edf53067be8161786b0558a6
Armandkay <a.kayiranga1@alustudent.com>
2024-11-01 03:15:27 -0700

    Run

--- interalearn_frontend/src/App.js
+++ interalearn_frontend/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import About from './pages/About'; // Example of another page
-import Contact from './pages/Contact'; // Example of another page
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services'; // New Service page
+import Blog from './pages/Blog'; // New Blog page
+import FAQ from './pages/FAQ'; // New FAQ page
 
 const App = () => {
   return (
@@ -14,6 +17,9 @@ const App = () => {
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
+          <Route path="/services" component={Services} /> {/* New route */}
+          <Route path="/blog" component={Blog} /> {/* New route */}
+          <Route path="/faq" component={FAQ} /> {/* New route */}
           {/* Add more routes as needed */}
         </Switch>
       </div>
