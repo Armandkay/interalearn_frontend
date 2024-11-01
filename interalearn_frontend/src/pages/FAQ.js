8c7800c6ddd90dce5ff368c8f4eac2a9c6bc8eee
Armandkay <a.kayiranga1@alustudent.com>
2024-11-01 03:19:05 -0700

    Blog

---
+++ interalearn_frontend/src/pages/FAQ.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FAQ = () => {
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <p>Answers to commonly asked questions.</p>
+    </div>
+  );
+};
+
+export default FAQ;
+
